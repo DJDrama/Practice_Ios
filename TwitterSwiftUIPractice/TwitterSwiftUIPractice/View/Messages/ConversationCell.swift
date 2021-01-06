1d947e1b4711a20bf6046b6c424b15b8caede7f3
DJDrama <putzzang@hotmail.com>
2021-01-06 16:19:52 +0900

    Building the Conversations View

---
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/View/Messages/ConversationCell.swift
@@ -0,0 +1,39 @@
+//
+//  ConversationCell.swift
+//  TwitterSwiftUIPractice
+//
+//  Created by jb on 2021/01/06.
+//
+
+import SwiftUI
+
+struct ConversationCell: View {
+    var body: some View {
+        VStack {
+            HStack(spacing: 12){
+                Image("venom-10")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(56/2)
+                
+                VStack(alignment: .leading, spacing: 4){
+                    Text("venom")
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text("Longer messages text to see what happens w hen i do this.")
+                        .font(.system(size: 15))
+                        .lineLimit(2)
+                }.padding(.trailing)
+            }
+            Divider()
+        }
+    }
+}
+
+struct ConversationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationCell()
+    }
+}
