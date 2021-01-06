5c8dd65f5607fca64c90742433257d635f302f1b
DJDrama <putzzang@hotmail.com>
2021-01-06 14:13:03 +0900

    Floating Action Button

---
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/View/FeedView.swift
@@ -0,0 +1,35 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUIPractice
+//
+//  Created by jb on 2021/01/06.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ZStack(alignment: .bottomTrailing){
+            ScrollView{
+                
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image("tweet")
+                    .resizable()
+                    .renderingMode(.template) //making foregroundcolor white
+                    .frame(width: 32, height: 32)
+                    .padding()
+            })
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .padding()
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
