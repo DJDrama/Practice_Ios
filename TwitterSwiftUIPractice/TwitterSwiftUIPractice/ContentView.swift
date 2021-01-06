9b046a444889731aaa3cd49073d09c6a89ba3684
DJDrama <putzzang@hotmail.com>
2021-01-06 13:15:10 +0900

    Swift UI TabView

---
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUIPractice
+//
+//  Created by jb on 2021/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            TabView{
+                Text("Feed")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                Text("Message")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Message")
+                    }
+            }
+            .navigationBarTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/TwitterSwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIPracticeApp.swift
+//  TwitterSwiftUIPractice
+//
+//  Created by jb on 2021/01/06.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
