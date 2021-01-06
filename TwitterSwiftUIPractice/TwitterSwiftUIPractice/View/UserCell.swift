9e91dfac229032be79919e8294bb44aa44960723
DJDrama <putzzang@hotmail.com>
2021-01-06 16:07:24 +0900

    User Cells

---
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/View/UserCell.swift
@@ -0,0 +1,35 @@
+//
+//  UserCell.swift
+//  TwitterSwiftUIPractice
+//
+//  Created by jb on 2021/01/06.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        HStack(spacing: 12){
+            Image("venom-10")
+                .resizable()
+                .scaledToFill()
+                .clipped()
+                .frame(width: 56, height: 56)
+                .cornerRadius(56/2)
+            
+            VStack(alignment: .leading, spacing: 4){
+                Text("venom")
+                    .font(.system(size: 14, weight: .semibold))
+                
+                Text("Unknown")
+                    .font(.system(size: 14))
+            }
+        }
+    }
+}
+
+struct UserCell_Previews: PreviewProvider {
+    static var previews: some View {
+        UserCell()
+    }
+}
