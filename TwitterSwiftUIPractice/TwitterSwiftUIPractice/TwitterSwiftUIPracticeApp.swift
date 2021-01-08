82d634744f5d42133d9a910636a6863fa7a1a982
DJDrama <putzzang@hotmail.com>
2021-01-08 15:31:05 +0900

    Building Registration View

--- TwitterSwiftUIPractice/TwitterSwiftUIPractice/TwitterSwiftUIPracticeApp.swift
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/TwitterSwiftUIPracticeApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TwitterSwiftUIPracticeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LoginView()
         }
     }
 }
--- TwitterSwiftUIPractice/TwitterSwiftUIPractice/View/Authentication/LoginView.swift
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/View/Authentication/LoginView.swift
@@ -12,69 +12,75 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        ZStack {
-            VStack{
-                Image("twitter-logo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 220, height: 100)
-                    .padding(.top, 96)
-                    .padding(.bottom, 32)
-                
-                VStack(spacing: 24){
-                    CustomTextField(text: $email, placeHolder: Text("Email"), imageName: "envelope")
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
+        NavigationView {
+            ZStack {
+                VStack{
+                    Image("twitter-logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 220, height: 100)
+                        .padding(.top, 96)
+                        .padding(.bottom, 32)
                     
-                    CustomSecureField(text: $password, placeHolder: Text("Password"))
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                }
-                .padding(.horizontal, 32)
-                
-                HStack{
-                    Spacer()
-                    Button(action: {
-                        
-                    }, label: {
-                        Text("Forgot Password?")
-                            .font(.footnote)
-                            .bold()
+                    VStack(spacing: 24){
+                        CustomTextField(text: $email, placeHolder: Text("Email"), imageName: "envelope")
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
                             .foregroundColor(.white)
-                            .padding(.top, 16)
-                            .padding(.trailing, 32)
+                        
+                        CustomSecureField(text: $password, placeHolder: Text("Password"))
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.horizontal, 32)
+                    
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Forgot Password?")
+                                .font(.footnote)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(.top, 16)
+                                .padding(.trailing, 32)
+                        })
+                    }
+                    
+                    Button(action: {}, label: {
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .frame(width: 360, height: 50)
+                            .background(Color.white)
+                            .clipShape(Capsule())
+                            .padding()
                     })
+                    
+                    Spacer()
+                    
+                    NavigationLink(
+                        destination: RegistrationView().navigationBarBackButtonHidden(true),
+                        label: {
+                            HStack{
+                                Text("Don't have an account?")
+                                    .font(.system(size: 14))
+                                Text("Sign Up")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            .foregroundColor(.white)
+                            .padding(.bottom, 40)
+                        })
                 }
                 
-                Button(action: {}, label: {
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .frame(width: 360, height: 50)
-                        .background(Color.white)
-                        .clipShape(Capsule())
-                        .padding()
-                })
-                
-                Spacer()
-                
-                HStack{
-                    Text("Don't have an account?")
-                        .font(.system(size: 14))
-                    Text("Sign Up")
-                        .font(.system(size: 14, weight: .semibold))
-                }
-                .foregroundColor(.white)
-                .padding(.bottom, 40)
             }
-            
+            .background(Color(#colorLiteral(red: 0.1119716689, green: 0.6307634115, blue: 0.9512073398, alpha: 1)))
+            .ignoresSafeArea()
         }
-        .background(Color(#colorLiteral(red: 0.1119716689, green: 0.6307634115, blue: 0.9512073398, alpha: 1)))
-        .ignoresSafeArea()
     }
 }
 
