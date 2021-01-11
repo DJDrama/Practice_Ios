015cbcb23a29869d98c9cdbe5b9b1a79ee781599
DJDrama <putzzang@hotmail.com>
2021-01-11 16:00:16 +0900

    Unfollow User

--- TwitterSwiftUIPractice/TwitterSwiftUIPractice/View/Profile/UserProfileView.swift
+++ TwitterSwiftUIPractice/TwitterSwiftUIPractice/View/Profile/UserProfileView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct UserProfileView: View {
     let user: User
     @ObservedObject var viewModel: ProfileViewModel
+    
     init(user: User){
         self.user = user
         self.viewModel   = ProfileViewModel(user: user)
